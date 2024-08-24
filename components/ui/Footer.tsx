14fd90c074b0f71459f43b99885abef2b35d0a1c
LanWebDev <149800661+LanWebDev@users.noreply.github.com>
2024-08-24 17:29:10 +0200

    updated the footer

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import logo from "@/assets/ReadWell.png";
 import Image from "next/image";
 import { footerIcons } from "@/constants/constants";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="bg-blue-200 p-10 flex justify-center items-center flex-col  ">
       <Image src={logo} alt="logo" height={70} />
       <p className="m-4">©2024 ReadWell. All Rights reserved</p>
+
       <div className="flex-row">
         {footerIcons.map((icon) => (
           <button key={icon.id} className="opacity-80 mx-4">
@@ -15,6 +17,17 @@ const Footer = () => {
           </button>
         ))}
       </div>
+      <p className="mt-4">
+        Built by{" "}
+        <Link
+          href={"https://github.com/LanWebDev"}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-[#2563eb]"
+        >
+          Lan Skrobar
+        </Link>
+      </p>
     </footer>
   );
 };
